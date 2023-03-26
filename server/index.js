683761a58a0c2b8122545b67a87f60d3bf42cbb8
Alexey928 <aleksey__tihakov@ukr.net>
2023-03-26 13:48:28 +0300

    port is started

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
 const express = require("express");
+
+
+
+
+const app = express();
+
+app.use(express.json());
+
+
+app.listen(4001,()=>{
+    console.log('port started #4001')});
+
+
+app.get("/",(req,res)=>{
+     res.status(200).json({masege:"working"});
+ });
+
+
